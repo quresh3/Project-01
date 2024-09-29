c599eb430d0491a501a7e4a79da4985620577aad
quresh3 <quresh3@rpi.edu>
2024-09-29 18:59:26 -0400

    Assets for workshop-04

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    public int a = 2;
+    public float b = 3f;
+    public Vector3 c = Vector3.zero;
+    public bool d = true;
+
+    public GameObject obj;
+    public Transform transform;
+
+    public Spinner spinner;
+    public Spawner spawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
